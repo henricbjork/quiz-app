350feb09b3b9861d747a46153717d567e8ef1320
Henric <henric.bjorkvall@grebban.com>
2021-12-17 11:23:39 +0100

    add ability to choose amount of questions

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import fetchQuizQuestions, { Difficulty, QuestionState } from './API';
 
 import QuestionCard from './components/QuestionCard';
 
-const TOTAL_QUESTIONS: number = 2;
-
 const difficulties: { label: string; value: Difficulty }[] = [
   { label: 'Easy', value: Difficulty.EASY },
   { label: 'Medium', value: Difficulty.MEDIUM },
   { label: 'Hard', value: Difficulty.HARD },
 ];
+
+const allowedAmountOfQuestions: number[] = [5, 10, 20];
 
 type AnswerObject = {
   correctAnswer: string;
@@ -21,18 +21,20 @@ type AnswerObject = {
 };
 
 interface State {
-  currentQuestion: number;
   activeDifficulty: Difficulty;
+  currentQuestion: number;
   gameOver: boolean;
   loading: boolean;
   questions: QuestionState[];
   score: number;
+  totalQuestions: number;
   userAnswers: AnswerObject[];
 }
 
 const initialState = {
-  currentQuestion: 0,
   activeDifficulty: Difficulty.UNDEFINED,
+  totalQuestions: 5,
+  currentQuestion: 0,
   gameOver: true,
   loading: false,
   questions: [],
@@ -49,15 +51,16 @@ const App = () => {
     loading,
     questions,
     score,
+    totalQuestions,
     userAnswers,
   } = state;
 
   console.log(activeDifficulty);
   const startTrivia = async (difficulty: Difficulty) => {
-    setState({ ...initialState, loading: true, gameOver: false });
+    setState((prevState) => ({ ...prevState, loading: true, gameOver: false }));
 
     const newQuestions = await fetchQuizQuestions(
-      TOTAL_QUESTIONS,
+      totalQuestions,
       activeDifficulty
     );
 
@@ -99,7 +102,7 @@ const App = () => {
   const nextQuestion = () => {
     const nextQuestion = currentQuestion + 1;
 
-    if (nextQuestion === TOTAL_QUESTIONS) {
+    if (nextQuestion === totalQuestions) {
       setState((prevState: State) => ({
         ...prevState,
         gameOver: true,
@@ -111,71 +114,94 @@ const App = () => {
       }));
     }
   };
+  console.log(totalQuestions);
 
   const showNextQuestionButton =
     !gameOver &&
     !loading &&
     userAnswers.length === currentQuestion + 1 &&
-    currentQuestion + 1 !== TOTAL_QUESTIONS;
-  const showRestartButton = userAnswers.length === TOTAL_QUESTIONS;
+    currentQuestion + 1 !== totalQuestions;
+  const showRestartButton = userAnswers.length === totalQuestions;
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <h1 className="mb-6 text-5xl">Quiz</h1>
-      <p className="mb-6">Choose difficulty</p>
-      <div className="flex mb-6">
-        {difficulties.map((difficulty) => {
-          const isActive = activeDifficulty === difficulty.value;
-
-          return (
-            <button
-              className={`mr-4 last:mr-0 py-1 px-6 border-2 border-solid ${
-                isActive ? 'border-blue-500 text-blue-500' : 'border-black'
-              } hover:border-blue-500 hover:text-blue-500`}
-              onClick={() => {
-                startTrivia(difficulty.value);
-              }}
-            >
-              {difficulty.label}
-            </button>
-          );
-        })}
-      </div>
-
-      {!gameOver && <p className="mb-6">Score: {score}</p>}
-      {loading && <p>Loading questions...</p>}
-      <div className="text-center min-h-[500px]">
-        {!loading && !gameOver && (
-          <QuestionCard
-            questionNumber={currentQuestion + 1}
-            totalQuestions={TOTAL_QUESTIONS}
-            question={questions[currentQuestion].question}
-            answers={questions[currentQuestion].answers}
-            userAnswer={userAnswers ? userAnswers[currentQuestion] : undefined}
-            callback={checkAnswer}
-          />
-        )}
-
-        {showNextQuestionButton && (
-          <button
-            className="mt-3 py-1 px-6 border-2 border-solid border-black hover:border-blue-500 hover:text-blue-500"
-            onClick={nextQuestion}
+      {gameOver ? (
+        <>
+          <p className="mb-6">How many questions do you want?</p>
+          <select
+            className="mb-6 py-1 pl-1 pr-3 border-2 border-solid border-black"
+            onChange={(e) =>
+              setState((prevState) => ({
+                ...prevState,
+                totalQuestions: parseInt(e.target.value, 10),
+              }))
+            }
           >
-            Next question
-          </button>
-        )}
-        {showRestartButton && (
-          <>
-            <p className="mb-4 mt-2 text-xl">Total score: {score}</p>
-            <button
-              className="py-1 px-6 border-2 border-solid border-black hover:border-blue-500 hover:text-blue-500"
-              onClick={() => startTrivia(activeDifficulty)}
-            >
-              Play again
-            </button>
-          </>
-        )}
-      </div>
+            {allowedAmountOfQuestions.map((amount) => {
+              return <option value={amount}>{amount}</option>;
+            })}
+          </select>
+          <p className="mb-6">Choose difficulty</p>
+          <div className="flex mb-6">
+            {difficulties.map((difficulty) => {
+              const isActive = activeDifficulty === difficulty.value;
+
+              return (
+                <button
+                  className={`mr-4 last:mr-0 py-1 px-6 border-2 border-solid ${
+                    isActive ? 'border-blue-500 text-blue-500' : 'border-black'
+                  } hover:border-blue-500 hover:text-blue-500`}
+                  onClick={() => {
+                    startTrivia(difficulty.value);
+                  }}
+                >
+                  {difficulty.label}
+                </button>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <>
+          <p className="mb-6">Score: {score}</p>
+          {loading && <p>Loading questions...</p>}
+          <div className="text-center min-h-[500px]">
+            {!loading && !gameOver && (
+              <QuestionCard
+                questionNumber={currentQuestion + 1}
+                totalQuestions={totalQuestions}
+                question={questions[currentQuestion].question}
+                answers={questions[currentQuestion].answers}
+                userAnswer={
+                  userAnswers ? userAnswers[currentQuestion] : undefined
+                }
+                callback={checkAnswer}
+              />
+            )}
+
+            {showNextQuestionButton && (
+              <button
+                className="mt-3 py-1 px-6 border-2 border-solid border-black hover:border-blue-500 hover:text-blue-500"
+                onClick={nextQuestion}
+              >
+                Next question
+              </button>
+            )}
+            {showRestartButton && (
+              <>
+                <p className="mb-4 mt-2 text-xl">Total score: {score}</p>
+                <button
+                  className="py-1 px-6 border-2 border-solid border-black hover:border-blue-500 hover:text-blue-500"
+                  onClick={() => setState(initialState)}
+                >
+                  Play again
+                </button>
+              </>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
